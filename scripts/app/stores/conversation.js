b13b6250874a3cdd6a2ddbba236aa067bc96f3ae
David Sinclair <david@sikhote.com>
2015-10-01 15:28:25 -0700

    fixing animation

--- scripts/app/stores/conversation.js
+++ scripts/app/stores/conversation.js
@@ -203,9 +203,7 @@ let Store = assign({}, events.EventEmitter.prototype, {
 		Talk.initialize(reponse => {this.initializeHandler(reponse);});
 	},
 	initializeHandler: function(response) {
-		console.log(response);
 		if(response.status && response.status !== 200) {
-			console.log('failed');
 			this.changeShowQuestions('down');
 
 			this.changeShowMessage(true,
@@ -216,7 +214,6 @@ let Store = assign({}, events.EventEmitter.prototype, {
 			storage.showAskAnother = false;
 			storage.showRetry = true;
 		} else {
-			console.log('success');
 			storage.questions =  response[0]? response[0] : [];
 			storage.location = response[1];
 			User.saveObject(sessionStorage, response[2]);
@@ -228,7 +225,6 @@ let Store = assign({}, events.EventEmitter.prototype, {
 
 		storage.isWaiting = false;
 		this.emitChange();
-		console.log(storage);
 	},
 	removeChangeListener: function(callback) {
 		this.removeListener(CHANGE_EVENT, callback);
